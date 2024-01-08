7e25107a0a6ec41ec1baff1ff536ae59f5f545d6
enzoluizb <enzoluiz.brt@gmail.com>
2024-01-08 13:54:32 -0300

    nomenclature

--- app/dist/js/decorators/inspect.js
+++ app/dist/js/decorators/inspect.js
@@ -1,8 +1,8 @@
 export function inspect() {
-    return function (target, property, descriptor) {
+    return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
-            console.log(`--- Método ${property}`);
+            console.log(`--- Método ${propertyKey}`);
             console.log(`------ parâmetros: ${JSON.stringify(args)}`);
             const retorno = metodoOriginal.apply(this, args);
             console.log(`------ retorno: ${JSON.stringify}`);
--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -1,12 +1,12 @@
 export function inspect() {
   return function (
     target: any,
-    property: string,
+    propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args: any[]) {
-        console.log(`--- Método ${property}`);
+        console.log(`--- Método ${propertyKey}`);
         console.log(`------ parâmetros: ${JSON.stringify(args)}`)
         const retorno = metodoOriginal.apply(this, args);
         console.log(`------ retorno: ${JSON.stringify}`)
